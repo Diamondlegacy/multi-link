cd6666c646a075649473d982ae6e09e11242c2de
Diamondlegacy <90890787+Diamondlegacy@users.noreply.github.com>
2026-08-03 08:43:33 +0100

    Add logo SVG to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,12 @@ export default function Navbar({ user, onLogout }) {
   return (
     <header className="navbar">
       <div className="navbar-brand">
-        <span className="navbar-mark">M</span>
+        <span className="navbar-mark">
+  <svg width="30" height="30" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+    <rect width="64" height="64" rx="14" fill="#E8A63C" />
+    <path d="M16 42 V22 L24 34 L32 22 L40 34 L48 22 V42" stroke="#12213A" strokeWidth="5" fill="none" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+</span>
         <span className="navbar-name">multi-link</span>
       </div>
       {user && (
